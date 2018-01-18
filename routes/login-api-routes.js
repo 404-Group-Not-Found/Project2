f27905fe8f036d28bfa0227d791d3bd207fca7ad
Jonathan Landrey Smith <jonathansmith@jonathans-mbp.uconnect.utah.edu>
2018-01-17 19:51:52 -0700

    login-api-route.js for login routes

---
+++ routes/login-api-routes.js
@@ -0,0 +1,31 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  app.get("/api/authors", function(req, res) {
+    // Here we add an "include" property to our options in our findAll query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
+    db.Author.findAll({
+      include: [db.Post]
+    }).then(function(dbAuthor) {
+      res.json(dbAuthor);
+    });
+  });
+
+  app.post("/api/authors", function(req, res) {
+    db.Author.create(req.body).then(function(dbAuthor) {
+      res.json(dbAuthor);
+    });
+  });
+
+  app.delete("/api/authors/:id", function(req, res) {
+    db.Author.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbAuthor) {
+      res.json(dbAuthor);
+    });
+  });
+  
+};
