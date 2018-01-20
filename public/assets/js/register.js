d82cb4b9e97f84f4e01bff5abca3f97f4a3c20ea
Andrew Ferrer <AndBFer@andrews-mbp.uconnect.utah.edu>
2018-01-20 12:41:06 -0700

    working on register and creating new user, making one new api route

--- public/assets/js/register.js
+++ public/assets/js/register.js
@@ -1,41 +1,69 @@
 $(document).ready(function() {
+    console.log("I am register.js");
 
-    var nameInput = $("#user");
-    var password = $("#pass");
+    // var nameInput = $("#user");
+    // var password = $("#pass");
     
-    $(document).on("#login", "#register-form", handleRegisterFormSubmit);
+    // $(document).on("#login", "#register-form", handleRegisterFormSubmit);
 
-    function handleRegisterFormSubmit(event) {
-        event.preventDfault();
+    // function handleRegisterFormSubmit(event) {
+    //     event.preventDfault();
 
-        if (!nameInput.val().trim().trim()) {
-            return;
-        }
+    //     if (!nameInput.val().trim().trim()) {
+    //         return;
+    //     }
 
-        upsertUser({
-            name: nameInput
-                .val()
-                .trim()
-        });
-    }
+    //     upsertUser({
+    //         name: nameInput
+    //             .val()
+    //             .trim()
+    //     });
+    // }
 
-    function upsertUser(userData) {
-        $.post("/api/users", userData)
-            .then();
-    }
+    //function to post the user to the database
+    // function upsertUser(userData) {
+    //     $.post("/api/users", userData)
+    //         .then();
+    // }
 
     var inputData;
 
+    //collect clean form data on submission and send to database
     $("#testSubmit").click(function(){
-        inputData = $('form').serializeArray();
-        console.log(inputData);
-    });
-    function testDisplay(inputData){
-        document.getElementById("testerDiv").innerHtml = inputData;
+        // inputData = $('form').serializeArray();
+        // console.log('This is inputData: ',inputData);
 
-    }
-        var test = document.getElementById("testSubmit");
-        test.addEventListener("click", testDisplay());
+        // for(var i =0, len = inputData.length;i<len;i++){
+        //     inputData[i] = $.trim(inputData[i]);
+        //   }
+        //   console.log(`This is trimmed inputData: ${inputData}`)
+        //   //turn it into a string if you wish
+        //   inputData = JSON.parse(inputData);
+        //   console.log(`This is string form of inputData: ${inputData}`);
+
+        event.preventDefault();
+        var newUser = {
+            name: $("#user").val().trim(),
+            password: $("#pass").val().trim()
+            };
+        $.post("/api/new", newUser)
+        // on success, run this callback
+        .done(function(data) {
+        // log the data we found
+        console.log(data);
+        // tell the user we're adding a character with an alert window
+        alert("Adding user...");
+        });
+        $("#name").val("");
+        $("#pass").val("");
+
+        function testDisplay(){
+            var displayData = JSON.stringify(newUser);
+            console.log(`This is DisplayData: ${displayData}`);
+            document.getElementById("testerDiv").innerHtml = displayData;
+        }
+    testDisplay();
+});
 
 
 
