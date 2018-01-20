74b949f76807e0d86193e4a0029554c74c8029cf
Jonathan Landrey Smith <jonathansmith@jonathans-mbp.uconnect.utah.edu>
2018-01-20 10:44:30 -0700

    keys.js removed

--- public/assets/js/keys.js
+++
@@ -1,7 +0,0 @@
-var zomato = require('zomato');
-
-var client = zomato.createClient({
-    userKey: '1a9d3c7e82a5c874f2f569af9f7c7d3b', //as obtained from [Zomato API](https://developers.zomato.com/apis) 
-});
-
-module.exports = client;
