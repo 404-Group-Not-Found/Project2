00e53e4280e8ca091e012d732fb89199918dc6e3
Andrew Ferrer <AndBFer@andrews-mbp.uconnect.utah.edu>
2018-01-20 12:45:17 -0700

    creating new users functional

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -27,9 +27,9 @@ module.exports = function(app) {
       });
     });
   
-    app.post("/api/authors", function(req, res) {
-      db.Author.create(req.body).then(function(dbAuthor) {
-        res.json(dbAuthor);
+    app.post("/api/new", function(req, res) {
+      db.User.create(req.body).then(function(dbUser) {
+        res.json(dbUser);
       });
     });
   
