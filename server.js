4fe3c94953a4d14549f95a19135122e2f7157a90
Andrew Ferrer <AndBFer@Andrews-MacBook-Pro.local>
2018-01-21 14:34:59 -0700

    removed force: true param from sequelize sync to start saving database data

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app)
 
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({}).then(function() {
     app.listen(PORT, function() {
       console.log("App listening on PORT " + PORT);
     });
