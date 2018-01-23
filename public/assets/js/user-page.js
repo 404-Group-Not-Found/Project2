5c37861cb6a0e3db2487861fd39a63f7554d9adf
Troy Shields <troy.leland.shields@gmail.com>
2018-01-22 21:13:35 -0700

    get swiping working

--- public/assets/js/user-page.js
+++ public/assets/js/user-page.js
@@ -1,20 +1,50 @@
 $(document).ready(function() {
-    var pullNextImage = function() {
-        // pull next image URL from API
-        var nextImgURL = "https://1.bp.blogspot.com/_qEbjiFbQWGM/TCBVlN3mkYI/AAAAAAAADCM/7CjYqUHwbgY/s1600/workshop_modell_0126.jpg";
+    // some hard coded images--these will come from the API instead of being hardcoded here
+    var current = 0;
+    var nextImgURLs = [
+        "https://1.bp.blogspot.com/_qEbjiFbQWGM/TCBVlN3mkYI/AAAAAAAADCM/7CjYqUHwbgY/s1600/workshop_modell_0126.jpg",
+        "http://static.stylemagazin.hu/medias/29280/Nem-ehezik-a-Women-of-the-Year-legjobb-modell-dijara-eselyes-szepseg_32fc7c86954a8847610499a0fc7261e2.jpg",
+        "http://w1nd.cc/promo/347.jpg",
+        "http://hircsarda.hu/wp-content/uploads/2016/03/orban1.jpeg"
+    ];
 
-        // use JQuery to create .buddy div with image element inside of it and place on DOM
-        
+    var pullNextImage = function(numToPull) {
+        // loop numToPull to get that many new images
+
+        for (var i = 0; i < numToPull; i++) {
+
+            // instead of using setTimeout and getting the img URL from the hardcoded array of nextImgURLs, you'll get the image from your API
+            setTimeout(function(){
+                /* REPLACE CODE BELOW HERE */
+                if (current > 3) {
+                    return;
+                }
+                nextImgURL = nextImgURLs[current];
+                current++
+                 /* REPLACE CODE ABOVE HERE */
+    
+                // Once you have the next image's URL you can call addTinderCard to create a new tinder card for it
+                var newSwipableCard = addTinderCard(nextImgURL);
+
+                // You can set data on the newSwipableCard if you want
+                newSwipableCard.attr("data-id", 10);
+                
+            }, 100);
+        }
     }
 
+    // create the first three cards
+    pullNextImage(3);
+
+    // initTinder to make the cards swipable
     initTinder(function(liked, element){
-        // Handle like or dislike of image
+        // Handle like or dislike of image however you want
         console.log("Liked?", liked, $(element).data("id"));
 
-        pullNextImage();
+        // add another card
+        pullNextImage(1);
     });
 
-    pullNextImage();
 })
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
